70c0d45551fa9a6881374c15383196ad4dbcbd2f
hinest <treverhines@gmail.com>
2016-05-17 23:28:11 -0400

    added more functionality to gcv

--- modest/gcv.py
+++ modest/gcv.py
@@ -14,7 +14,7 @@ def product_trace(A,B):
   return np.sum(A*B.T)
 
 
-def predictive_error(alpha,A,L,data):
+def gcv_predictive_error(alpha,A,L,data):
   ''' 
   solves 
 
@@ -34,12 +34,11 @@ def predictive_error(alpha,A,L,data):
     # stable because singular values are removed
     ATA = A.T.dot(A)
     LTL = alpha**2*L.T.dot(L)
-    A_ginv = inv(ATA + LTL).dot(A.T)
-    #A_ginv = pinv(ATA + LTL,rcond=1e-12).dot(A.T)
+    A_ginv = pinv(ATA + LTL).dot(A.T)
   except np.linalg.linalg.LinAlgError:
     # if the generalized inverse cant be computed then the predictive
     # error is infinite
-    print('WARNING: failed to compute generalized inverse')
+    print('WARNING: failed to compute generalized inverse with damping parameter %s' % alpha)
     return np.inf
 
   # estimate m
@@ -56,11 +55,43 @@ def predictive_error(alpha,A,L,data):
   return numerator/denominator
 
 
-def _log_predictive_error(log_alpha,A,L,data):
-  return predictive_error(10**log_alpha,A,L,data)
+def cv_predictive_error(alpha,A,L,data):
+  ''' 
+  returns the predictive error using cross validation
+  '''
+  N,M = A.shape  
+  K = L.shape[0]
+  residual = np.zeros(N)
+  # allocate an array for stacked A and L matrices
+  A_ext = np.zeros((N-1+K,M))
+  A_ext[N-1:,:] = alpha*L
+  data_ext = np.zeros(N+K-1)
+  for removed_idx in range(N):
+    kept_idx = np.delete(np.arange(N),removed_idx)
+    A_ext[:N-1,:] = A[kept_idx,:]
+    data_ext[:N-1] = data[kept_idx]
+    # estimated model with remove data point
+    mest = np.linalg.lstsq(A_ext,data_ext)[0]
+    # prediction for the removed data point
+    pred = A[removed_idx,:].dot(mest)
+    residual[removed_idx] = pred - data[removed_idx]     
+
+  return residual.dot(residual)/N
+
+@funtime
+def predictive_error(alpha,A,L,data,gcv=True):
+  if gcv:
+    return gcv_predictive_error(alpha,A,L,data)
+  else:
+    return cv_predictive_error(alpha,A,L,data)
 
 
-def optimal_damping(A,L,data,rcond=1e-12,plot=False):
+def _log_predictive_error(log_alpha,A,L,data,gcv=True):
+  return predictive_error(10**log_alpha,A,L,data,gcv=gcv)
+
+
+@funtime
+def optimal_damping(A,L,data,plot=False,gcv=False,log_bounds=None,itr=100):
   ''' 
   returns the optimal penalty parameter for regularized least squares 
   using generalized cross validation
@@ -73,32 +104,25 @@ def optimal_damping(A,L,data,rcond=1e-12,plot=False):
     plot: whether to plot the predictive error curve
 
   '''
-  # search in logspace
-  optimal_logalpha = scipy.optimize.minimize_scalar(
-                       _log_predictive_error,
-                       args=(A,L,data)).x
-  optimal_alpha = 10**optimal_logalpha
+  if log_bounds is None:
+    log_bounds = (-6.0,6.0)
 
+  alpha_range = 10**np.linspace(log_bounds[0],log_bounds[1],itr)
+  # predictive error for all tested damping parameters
+  err = np.array([predictive_error(a,A,L,data,gcv=gcv) for a in alpha_range])
+  optimal_alpha = alpha_range[np.argmin(err)]
+  optimal_err = np.min(err)
   if plot:
-    optimal_err = predictive_error(optimal_alpha,A,L,data)
-
-    # test damping parameters in log space
-    logalpha = np.log10(optimal_alpha)
-    logalpha_min = logalpha - 3  
-    logalpha_max = logalpha + 3  
-    alpha_range = 10**np.linspace(logalpha_min,logalpha_max,100)
-    # predictive error for all tested damping parameters
-    err = np.array([predictive_error(a,A,L,data) for a in alpha_range])
-
-    current_ax = plt.gca()
     fig,ax = plt.subplots()
-    ax.set_title('GCV curve')
+    if gcv:
+      ax.set_title('GCV curve')
+    else:
+      ax.set_title('CV curve')
     ax.set_ylabel('predictive error')
     ax.set_xlabel('penalty parameter')
     ax.loglog(alpha_range,err,'k-')
     ax.loglog(optimal_alpha,optimal_err,'ko',markersize=10) 
     ax.grid()
-    plt.sca(current_ax)
     
   return optimal_alpha
       
